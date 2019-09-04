cc87f97aa4e6eadd40d64ae807ca869d8cb258cc
piccolo <kabiryusufbashir@gmail.com>
2019-09-04 15:45:38 -0700

    commented the whole page, will start creating my blog

--- myFirst.js
+++ myFirst.js
@@ -1,12 +1,15 @@
-var message = "This is my first javascript";
-var number = 6;
-var boolean = true;
+//var message = "This is my first javascript";
+//var number = 6;
+//var boolean = true;
 
-var a = 5;
+//var a = 5;
 
-for(var x=1; x<=a; x++){
-  console.log(a);
-}
+//var myfunction = function(a, b){
+  //c = a + b;
+  //return c;
+//}
+
+//console.log
 
 /*if(a === b){
   console.log(true);
