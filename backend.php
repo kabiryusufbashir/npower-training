977efa88c861a217e3afb55030d13e83ebda5668
piccolo <kabiryusufbashir@gmail.com>
2019-09-05 11:05:13 -0700

    created a for loop to count 1 -100

--- backend.php
+++ backend.php
@@ -4,4 +4,10 @@
       foreach ($fruits as $type) {
         echo 'I love : '.$type.'<br>';
       }
+
+      echo '<h1>Counting Numbers 1 - 100</h1>';
+
+      for($x=1; $x<=100; $x++){
+        echo $x.' ';
+      }
  ?>
