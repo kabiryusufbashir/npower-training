31184ca42e75fc18dacc34143d856d87993b9f3e
piccolo <kabiryusufbashir@gmail.com>
2019-09-05 11:22:24 -0700

    Added a class col-fill to each section

--- backend.php
+++ backend.php
@@ -1,18 +1,28 @@
+<link rel="stylesheet" href="./style.css">
+
 <?php
   $fruits = array('mango', 'banana', 'apple', 'orange');
     $fruitsLen = count($fruits);
+
+      echo '<div class="col-fill">'
 
       echo '<h1>Friuts I love!!!</h1>';
 
       foreach ($fruits as $type) {
         echo 'I love : '.$type.'<br>';
       }
+      echo '</div>';
+
+      echo '<div class="col-fill">'
 
       echo '<h1>Counting Numbers 1 - 100</h1>';
 
       for($x=1; $x<=100; $x++){
         echo $x.' ';
       }
+      echo '</div>';
+
+      echo '<div class="col-fill">'
 
       echo '<h1>Functions</h1>';
 
@@ -23,4 +33,6 @@
 
       $result = addFunction(200, 50);
       echo $result;
+
+      echo '</div>';
  ?>
