2cd7fbeb2e5d6bdfe67f11bfb18b1cc9c2b51ece
piccolo <kabiryusufbashir@gmail.com>
2019-09-05 10:53:05 -0700

    Started Arrys

--- backend.php
+++ backend.php
@@ -1,6 +1,6 @@
 <?php
-  $boys = array('yusuf', 'bola', 'hassan');
-    foreach ($boys as $boy) {
-      echo 'The name of the boy is: '.$boy[0];
-    }
+
+  $fruits = array('mango', 'banana', 'apple', 'orange');
+      
+
  ?>
