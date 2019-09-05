da40a22a3629cdd36c1e50687ced8f849687a5bc
piccolo <kabiryusufbashir@gmail.com>
2019-09-05 11:19:15 -0700

    created a heading for the friuts i love

--- backend.php
+++ backend.php
@@ -2,7 +2,7 @@
   $fruits = array('mango', 'banana', 'apple', 'orange');
     $fruitsLen = count($fruits);
 
-    echo '<h1>Friuts I love!!!</h1>';
+      echo '<h1>Friuts I love!!!</h1>';
 
       foreach ($fruits as $type) {
         echo 'I love : '.$type.'<br>';
@@ -13,4 +13,14 @@
       for($x=1; $x<=100; $x++){
         echo $x.' ';
       }
+
+      echo '<h1>Functions</h1>';
+
+      function addFunction($num1, $num2){
+        $sum = $num1 + $num2;
+        return 'The Sum is: '.$sum;
+      }
+
+      $result = addFunction(200, 50);
+      echo $result;
  ?>
