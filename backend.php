de172f9b4fa6ebd322a7bda151fd35a4ff197836
piccolo <kabiryusufbashir@gmail.com>
2019-09-05 11:01:14 -0700

    Used the foreach to list the name of the fruits I loved

--- backend.php
+++ backend.php
@@ -1,6 +1,7 @@
 <?php
-
   $fruits = array('mango', 'banana', 'apple', 'orange');
-      
-
+    $fruitsLen = count($fruits);
+      foreach ($fruits as $type) {
+        echo 'I love : '.$type.'<br>';
+      }
  ?>
