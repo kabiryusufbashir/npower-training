d45a2fe1d5f135b40b024f9aff42a3b53ef82c81
piccolo <kabiryusufbashir@gmail.com>
2019-09-06 09:19:03 -0700

    Added the form structure

---
+++ to-do/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>To Do List</title>
+  </head>
+  <body>
+    <h1>To Do APP</h1>
+    <form action="index.html" method="post">
+      <input type="text" name="add" placeholder="Add a To Do">
+      <input type="submit" name="submit" value="Add">
+      
+    </form>
+  </body>
+</html>
+<?php
+
+?>
