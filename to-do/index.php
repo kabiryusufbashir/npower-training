493f68b771a9e0e21ea475a8bc64979145b6444e
piccolo <kabiryusufbashir@gmail.com>
2019-09-06 09:44:20 -0700

    Created the connection to the database

--- to-do/index.php
+++ to-do/index.php
@@ -1,3 +1,12 @@
+<?php
+  $conn = mysqli_connect('localhost', 'root', 'K@rn@ugh123', 'npower_training');
+    if($conn->connect_error){
+      die('Database Connection Failed');
+    }else{
+      die('We are connected');
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
