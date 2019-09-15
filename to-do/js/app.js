1a57fc4afe574d3937a0e6f07eca1bad92ecf35e
piccolo <kabiryusufbashir@gmail.com>
2019-09-15 09:54:31 -0700

    trying to figure it out...

--- to-do/js/app.js
+++ to-do/js/app.js
@@ -1,16 +1,26 @@
 let myFormToDo = document.getElementById('myFormToDo');
 let display = document.getElementById('display');
 let toDo = document.getElementById('whatToDo');
+let whatToDoAdd = document.getElementById('whatToDoAdd');
+
   whatToDoAdd.onclick = function(){
     toDo = toDo.value;
     if(toDo == ""){
       alert('Not');
     }else{
-      newDiv = document.createElement('p');
-      newSpan = document.createElement('span');
+      let newDiv = document.createElement('p');
+      let newSpan = document.createElement('span');
+      let newBtn = document.createElement('button');
+
         newSpan.innerHTML = toDo;
-        newDiv.appendChild(newSpan);
+
+        newSpanBtn.setAttribute("name", "del");
+        newSpanBtn.innerHTML = 'Delete';
+
         display.appendChild(newDiv);
+        newDiv.appendChild(newSpan);
+        newSpan.appendChild(newBtn);
+
         let position = document.getElementsByTagName('p')[0];
           display.insertBefore(newDiv, position);
     }
