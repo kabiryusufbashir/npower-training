24a890e888ce7abeafb87e9654927aec749a8c0b
piccolo <kabiryusufbashir@gmail.com>
2019-09-15 16:41:09 -0700

    figure out add, works perfect but the delete button has a problem

--- to-do/js/app.js
+++ to-do/js/app.js
@@ -4,19 +4,44 @@ let toDo = document.getElementById('whatToDo');
 let whatToDoAdd = document.getElementById('whatToDoAdd');
 let delAdd = document.querySelectorAll('#display .delete');
 
-  const addForm = document.forms['whatToDoAdd'];
+  const addForm = document.forms['myFormToDo'];
+
     addForm.addEventListener('submit', function(e){
+
       e.preventDefault();
-        alert('Hello');
+
+      const value = addForm.querySelector('input[type="text"]').value;
+
+          if(value == ""){
+            alert('ToDo Field empty');
+          }else{
+            let newDiv = document.createElement('p');
+            let newSpan = document.createElement('span');
+            let newBtn = document.createElement('button');
+
+              newSpan.textContent = value;
+              newBtn.textContent = 'Delete';
+
+              newBtn.setAttribute("name", "del");
+              newBtn.setAttribute("class", "delete");
+
+              display.appendChild(newDiv);
+              newDiv.appendChild(newSpan);
+              newSpan.appendChild(newBtn);
+
+              let position = document.getElementsByTagName('p')[0];
+                display.insertBefore(newDiv, position);
+          }
+
     });
 
-  // Array.from(delAdd).forEach(function(btn){
-  //   btn.addEventListener('click', function(e){
-  //     e.preventDefault();
-  //     const p = e.target.parentElement;
-  //     p.parentNode.removeChild(p);
-  //   });
-  // });
+  Array.from(delAdd).forEach(function(btn){
+    btn.addEventListener('click', function(e){
+      e.preventDefault();
+      const p = e.target.parentElement;
+      p.parentNode.removeChild(p);
+    });
+  });
 
    // whatToDoAdd.addEventListener('click', function(e){
    //   e.preventDefault();
