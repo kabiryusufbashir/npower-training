91df8f5b453a4bc126c7b5831f405d80ef40c4ae
piccolo <kabiryusufbashir@gmail.com>
2019-09-15 16:28:37 -0700

    testing ninja video

--- to-do/js/app.js
+++ to-do/js/app.js
@@ -1,36 +1,44 @@
-//let myFormToDo = document.getElementById('myFormToDo');
+let myFormToDo = document.getElementById('myFormToDo');
 let display = document.getElementById('display');
 let toDo = document.getElementById('whatToDo');
 let whatToDoAdd = document.getElementById('whatToDoAdd');
 let delAdd = document.querySelectorAll('#display .delete');
 
-  Array.from(delAdd).forEach(function(btn){
-    btn.addEventListener('click', function(e){
-      const p = e.target.parentElement;
-      p.parentNode.removeChild(p);
+  const addForm = document.forms['whatToDoAdd'];
+    addForm.addEventListener('submit', function(e){
+      e.preventDefault();
+        alert('Hello');
     });
-  });
 
-   whatToDoAdd.addEventListener('click', function(e){
-     toDo = toDo.value;
-       if(toDo == ""){
-         alert('Not');
-       }else{
-         let newDiv = document.createElement('p');
-         let newSpan = document.createElement('span');
-         let newBtn = document.createElement('button');
+  // Array.from(delAdd).forEach(function(btn){
+  //   btn.addEventListener('click', function(e){
+  //     e.preventDefault();
+  //     const p = e.target.parentElement;
+  //     p.parentNode.removeChild(p);
+  //   });
+  // });
 
-           newSpan.innerHTML = toDo;
-
-           newBtn.setAttribute("name", "del");
-           newBtn.setAttribute("class", "delete");
-           newBtn.innerHTML = 'Delete';
-
-           display.appendChild(newDiv);
-           newDiv.appendChild(newSpan);
-           newSpan.appendChild(newBtn);
-
-           let position = document.getElementsByTagName('p')[0];
-             display.insertBefore(newDiv, position);
-       }
-   });
+   // whatToDoAdd.addEventListener('click', function(e){
+   //   e.preventDefault();
+   //   toDo = toDo.value;
+   //     if(toDo == ""){
+   //       alert('Not');
+   //     }else{
+   //       let newDiv = document.createElement('p');
+   //       let newSpan = document.createElement('span');
+   //       let newBtn = document.createElement('button');
+   //
+   //         newSpan.innerHTML = toDo;
+   //
+   //         newBtn.setAttribute("name", "del");
+   //         newBtn.setAttribute("class", "delete");
+   //         newBtn.innerHTML = 'Delete';
+   //
+   //         display.appendChild(newDiv);
+   //         newDiv.appendChild(newSpan);
+   //         newSpan.appendChild(newBtn);
+   //
+   //         let position = document.getElementsByTagName('p')[0];
+   //           display.insertBefore(newDiv, position);
+   //     }
+   // });
