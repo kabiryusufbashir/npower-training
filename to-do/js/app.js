4796afa9dd3e132bbf38742fb1e6d45c4bf9cd36
piccolo <kabiryusufbashir@gmail.com>
2019-09-14 21:39:07 -0700

    started with JS file

---
+++ to-do/js/app.js
@@ -0,0 +1,13 @@
+let whatToDoAdd = document.getElementById('whatToDoAdd');
+  whatToDoAdd.onclick = function(){
+    if(whatToDoAdd.length > 0){
+      alert('Okay');
+      //let whatToDo = document.getElementById('whatToDo');
+      //let display = document.getElementById('display');
+        //let newSpan = document.createElement('span');
+          //display.appendChild(newSpan);
+    }else{
+      alert('What to Add field is empty');
+    }
+
+  }
