30a1bb867d05937b03f88ac17cbe2fdb88d584da
piccolo <kabiryusufbashir@gmail.com>
2019-09-14 21:59:20 -0700

    finished writing the code

--- to-do/js/app.js
+++ to-do/js/app.js
@@ -1,9 +1,16 @@
 let myFormToDo = document.forms.myFormToDo;
+let display = document.getElementById('display');
 let toDo = myFormToDo.whatToDo;
   myFormToDo.onsubmit = function(){
-    if(toDo.value == ""){
+    toDo = toDo.value;
+    if(toDo == ""){
       alert('Not');
     }else{
-      alert('Yes');
+      newDiv = document.createElement('div');
+      newSpan = document.createElement('span');
+        display.appendChild(newDiv);
+        newDiv.appendChild(newSpan);
+        newSpan.innerHTML = toDo;
+          display.insertBefore(newDiv, display.getElementsByTagName('div'));
     }
   }
